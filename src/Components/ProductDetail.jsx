b6045c411717a97d0fb24d23843d4769f1ddec01
milderz <75046268+milderz@users.noreply.github.com>
2022-12-24 14:50:59 -0600

    Fix error: Line 14:18:  'setProducts' is assigned a value but never used  no-unused-vars

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -4,14 +4,18 @@ import ProductDetailCard from './ProductDetailCard'
 import { StyledProductDetail } from './Styles/ProductDetail.styled'
 import { useParams } from 'react-router-dom'
 import ProductData from "../products.json";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const ProductDetail = () => {
 
  let { name } = useParams()
 
-const [products, setProducts] = useState(ProductData);
+const [products, setProducts] = useState();
+
+useEffect(() => {
+    setProducts(ProductData);
+  }, []);
 
 const productDetail = products.filter((product) => product.slug === name)
 
