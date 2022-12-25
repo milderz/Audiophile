5cc79301c184ec760464f73409c0ebd5698fa690
milderz <75046268+milderz@users.noreply.github.com>
2022-12-25 11:20:47 -0600

    removed react favicons

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -1,30 +1,27 @@
-
-import PreviousPage from './PreviousPage'
-import ProductDetailCard from './ProductDetailCard'
-import { StyledProductDetail } from './Styles/ProductDetail.styled'
-import { useParams } from 'react-router-dom'
+import PreviousPage from "./PreviousPage";
+import ProductDetailCard from "./ProductDetailCard";
+import { StyledProductDetail } from "./Styles/ProductDetail.styled";
+import { useParams } from "react-router-dom";
 import ProductData from "../products.json";
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
 
 const ProductDetail = () => {
+  let { name } = useParams();
 
- let { name } = useParams()
+  const [products, setProducts] = useState([]);
 
-const [products, setProducts] = useState([]);
-
-useEffect(() => {
+  useEffect(() => {
     setProducts(ProductData);
   }, []);
 
-const productDetail = products.filter((product) => product.slug === name)
+  const productDetail = products.filter((product) => product.slug === name);
 
   return (
     <StyledProductDetail>
       <PreviousPage />
-      <ProductDetailCard productDetail={productDetail}/>
+      <ProductDetailCard productDetail={productDetail} />
     </StyledProductDetail>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
--- src/Components/ProductDetailCard.jsx
+++ src/Components/ProductDetailCard.jsx
@@ -1,32 +1,37 @@
-import PrimaryBtn from './PrimaryBtn'
-import QuantityBtn from './QuantityBtn'
-import { StyledProductDetailCard } from './Styles/ProductDetailCard.styled'
+import PrimaryBtn from "./PrimaryBtn";
+import QuantityBtn from "./QuantityBtn";
+import { StyledProductDetailCard } from "./Styles/ProductDetailCard.styled";
 
-
-const ProductDetailCard = ({productDetail}) => {
-
- productDetail.map((pr) => console.log(pr.image.desktop))
+const ProductDetailCard = ({ productDetail }) => {
+  productDetail.map((pr) => console.log(pr.image.desktop));
   return (
-
-    
     <>
-     {productDetail.map((product) => product && (<StyledProductDetailCard key={product.id} imgDesktop={product.image.desktop}>
-     <div  className="img-content"></div>
-     <div className="text-content">
-      {product.new && (<p className="headline">NEW PRODUCT</p>)}
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <p className="price">{`$${product.price}`}</p>
-      <div className="btn-wrapper">
-      <QuantityBtn />
-      <PrimaryBtn version="main" text="ADD TO CART"/>
-      </div>
-     </div></StyledProductDetailCard>))}
+      {productDetail.map(
+        (product) =>
+          product && (
+            <>
+              <StyledProductDetailCard
+                key={product.id}
+                imgDesktop={product.image.desktop}
+              >
+                <div className="img-content"></div>
+                <div className="text-content">
+                  {product.new && <p className="headline">NEW PRODUCT</p>}
+                  <h3>{product.name}</h3>
+                  <p>{product.description}</p>
+                  <p className="price">{`$${product.price}`}</p>
+                  <div className="btn-wrapper">
+                    <QuantityBtn />
+                    <PrimaryBtn version="main" text="ADD TO CART" />
+                  </div>
+                </div>
+              </StyledProductDetailCard>
+              <div></div>
+            </>
+          )
+      )}
     </>
-     
-      
-    
-  )
-}
+  );
+};
 
-export default ProductDetailCard
+export default ProductDetailCard;
