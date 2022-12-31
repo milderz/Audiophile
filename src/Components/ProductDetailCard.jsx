7aa7db45522ce6b35e27ad358d8fdf9e03606e88
milderz <75046268+milderz@users.noreply.github.com>
2022-12-31 11:09:02 -0600

    Updating product detail page

---
+++ src/Components/ProductDetailCard.jsx
@@ -0,0 +1,35 @@
+import PrimaryBtn from "./PrimaryBtn";
+import QuantityBtn from "./QuantityBtn";
+import { StyledProductDetailCard } from "./Styles/ProductDetailCard.styled";
+
+const ProductDetailCard = ({ productDetail }) => {
+  return (
+    <>
+      {productDetail.map(
+        (product) =>
+          product && (
+            <>
+              <StyledProductDetailCard
+                key={product.id}
+                imgDesktop={product.image.desktop}
+              >
+                <div className="img-content"></div>
+                <div className="text-content">
+                  {product.new && <p className="headline">NEW PRODUCT</p>}
+                  <h3>{product.name}</h3>
+                  <p>{product.description}</p>
+                  <p className="price">{`$${product.price}`}</p>
+                  <div className="btn-wrapper">
+                    <QuantityBtn />
+                    <PrimaryBtn version="main" text="ADD TO CART" />
+                  </div>
+                </div>
+              </StyledProductDetailCard>
+            </>
+          )
+      )}
+    </>
+  );
+};
+
+export default ProductDetailCard;
--- src/Components/ProductNav.jsx
+++ src/Components/ProductNav.jsx
@@ -1,15 +1,14 @@
-import { StyledProductNav } from "./Styles/ProductNav.styled"
+import { StyledProductNav } from "./Styles/ProductNav.styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const ProductNav = ({mobileNav, navActive}) => {
+const ProductNav = ({ mobileNav, navActive }) => {
   return (
     <StyledProductNav mobileNav={mobileNav} navActive={navActive}>
       <ul>
-       <li>
-        
-         <Link to="/headphones">
+        <li>
+          <Link to="/headphones">
             <img
               src="/assets/shared/desktop/image-headphones.png"
               alt="heaphones-link"
@@ -20,10 +19,9 @@ const ProductNav = ({mobileNav, navActive}) => {
               <FontAwesomeIcon icon={faAngleRight} />
             </p>
           </Link>
-       </li>
-       <li>
-        
-         <Link to="/speakers">
+        </li>
+        <li>
+          <Link to="/speakers">
             <img
               src="/assets/shared/desktop/image-speakers.png"
               alt="speakers-link"
@@ -34,10 +32,9 @@ const ProductNav = ({mobileNav, navActive}) => {
               <FontAwesomeIcon icon={faAngleRight} />
             </p>
           </Link>
-       </li>
-       <li>
-        
-         <Link to="/earphones">
+        </li>
+        <li>
+          <Link to="/earphones">
             <img
               src="/assets/shared/desktop/image-earphones.png"
               alt="earphones-link"
@@ -48,10 +45,10 @@ const ProductNav = ({mobileNav, navActive}) => {
               <FontAwesomeIcon icon={faAngleRight} />
             </p>
           </Link>
-       </li>
+        </li>
       </ul>
     </StyledProductNav>
-  )
-}
+  );
+};
 
-export default ProductNav
+export default ProductNav;
